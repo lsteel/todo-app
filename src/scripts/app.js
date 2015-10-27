5e3ba9b338dba7d45ce003296015f21fda488050
Lorin Steel <lorinsteel@gmail.com>
2015-10-27 11:15:51 -0600

    etc.

---
+++ src/scripts/app.js
@@ -0,0 +1,16 @@
+angular
+  .module('TodoApp', [
+    'ngRoute',
+    'LoginController'
+  ])
+  .config([
+    '$routeProvider',
+    function ($routeProvider) {
+      $routeProvider
+        .when('/login', {
+          templateUrl: '/partials/login-controller.html',
+          controller: 'LoginController',
+        })
+        .otherwise('/login');
+    }
+  ]);
